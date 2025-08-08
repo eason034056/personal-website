ce3471bd414b7bb875171154fd58463dab7b43fd
eason034056 <129682160+eason034056@users.noreply.github.com>
2025-08-08 23:28:17 +0800

    add openGraph photo

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -10,4 +10,26 @@ export const metadata: Metadata = {
     shortcut: '/images/icon.svg',
     apple: '/images/icon.svg',
   },
+  openGraph: {
+    title: 'Eason Wu - Portfolio',
+    description: 'Eason Wu - Portfolio',
+    url: 'https://www.easonwu.com',
+    siteName: 'Eason Wu Portfolio',
+    images: [
+      {
+        url: '/images/icon.svg',  // 您需要添加這張圖片
+        width: 630,
+        height: 630,
+        alt: 'Eason Wu Portfolio Preview'
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Eason Wu - Portfolio',
+    description: 'Eason Wu - Portfolio',
+    images: ['/images/icon.svg'],  // 使用相同的圖片
+  },
 }
